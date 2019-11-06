918f59d6c3612075b1629631067f1cfe8d48cb43
mykalrudie <mykalrudie@gmail.com>
2019-11-06 11:08:48 -0800

    Done. Maybe add a few more tests

--- src/main/java/evolution/Evolution.java
+++ src/main/java/evolution/Evolution.java
@@ -1,9 +1,13 @@
 package evolution;
 
+import com.sun.source.tree.BinaryTree;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.TreeVisitor;
+
+import java.util.HashSet;
 import java.util.Set;
 
 public class Evolution {
-
     /**
      * Compute the number of cells that survive the evolutionary process as
      * described in the problem statement assuming that a given cell dies during
@@ -22,10 +26,107 @@ public class Evolution {
      *
      * @return the number of surviving cells in the mature organism
      */
+
+
     public static int numSurvivingCells(int[] evolutionaryTree, int deadCell) {
-        // TODO: Implement this method
-        return 0; // change this
+        Set<Integer> survivors = new HashSet<>();
+        int count = 0;
+        Node n =  new Node(-1);
+
+        if (deadCell == 0) {
+            return 0;
+        }
+
+        for (int i = 0; i < evolutionaryTree.length; i++) {
+            if (evolutionaryTree[i] == -1) {
+                n = new Node(i);
+                makeTree(n, evolutionaryTree);
+            }
+        }
+
+        killCell(deadCell, n);
+        countCells(n, survivors);
+
+
+
+
+
+
+
+
+
+
+        return survivors.size();
     }
+
+    private static void killCell(int index, Node root) {
+        boolean sentinel = true;
+        if (root.left != null) {
+            if (root.left.value == index) {
+                root.left = null;
+                sentinel = false;
+            }
+        }
+        if (root.right != null) {
+            if (root.right.value == index) {
+                root.right = null;
+                sentinel = false;
+            }
+        }
+
+
+        if (sentinel) {
+            if (root.left != null) {
+                killCell(index, root.left);
+            }
+            if (root.right != null) {
+                killCell(index, root.right);
+            }
+        }
+    }
+
+    private static void countCells(Node root, Set<Integer> survivors) {
+        if (root.left == null && root.right == null) {
+            survivors.add(root.value);
+        } else {
+            if (root.left != null) {
+                countCells(root.left, survivors);
+            }
+            if (root.right != null) {
+                countCells(root.right, survivors);
+            }
+        }
+    }
+
+    private static void makeTree(Node root, int[] evolutionaryTree) {
+        for (int i = 0; i < evolutionaryTree.length; i++) {
+            if (evolutionaryTree[i] == root.value) {
+                Node n = new Node(i);
+                if (root.left == null) {
+                    root.left = n;
+                } else if (root.right == null) {
+                    root.right = n;
+                }
+             makeTree(n, evolutionaryTree);
+            }
+        }
+
+    }
+
+
+    private static class Node { //Source: https://www.baeldung.com/java-binary-tree
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+            right = null;
+            left = null;
+        }
+    }
+
+
 
     /**
      * Compute the number of cells that survive the evolutionary process as
@@ -47,8 +148,26 @@ public class Evolution {
      */
     public static Set<Integer> getSurvivingCells(int[] evolutionaryTree,
                                                  int deadCell) {
-        // TODO: Implement this method
-        return null; // change this
+        Set<Integer> survivors = new HashSet<>();
+
+        Node n =  new Node(-1);
+
+        if (deadCell == 0) {
+            return survivors;
+        }
+
+        for (int i = 0; i < evolutionaryTree.length; i++) {
+            if (evolutionaryTree[i] == -1) {
+                n = new Node(i);
+                makeTree(n, evolutionaryTree);
+            }
+        }
+
+        killCell(deadCell, n);
+        countCells(n, survivors);
+
+
+        return survivors; // change this
     }
 
 }
--- src/test/java/evolution/EvolutionTest.java
+++ src/test/java/evolution/EvolutionTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class EvolutionTest {
 
     @Test
@@ -44,6 +47,45 @@ public class EvolutionTest {
         assertEquals(14,
                 Evolution.numSurvivingCells(evolutionaryTree, deadCell));
     }
+
+    @Test
+    public void test1Set() {
+        int[] evolutionaryTree = { -1, 0, 0, 1, 1 };
+        int deadCell = 2;
+        Set<Integer> testSet = new HashSet<>();
+        testSet.add(3);
+        testSet.add(4);
+        assertEquals(testSet, Evolution.getSurvivingCells(evolutionaryTree, deadCell));
+    }
+
+    @Test
+    public void test2Set() {
+        int[] evolutionaryTree = { -1, 0, 0, 1, 1 };
+        int deadCell = 1;
+        Set<Integer> testSet = new HashSet<>();
+        testSet.add(2);
+        assertEquals(testSet, Evolution.getSurvivingCells(evolutionaryTree, deadCell));
+    }
+
+    @Test
+    public void test3Set() {
+        int[] evolutionaryTree = { -1, 0, 0, 1, 1 };
+        int deadCell = 0;
+        Set<Integer> testSet = new HashSet<>();
+        assertEquals(testSet, Evolution.getSurvivingCells(evolutionaryTree, deadCell));
+    }
+
+    @Test
+    public void test4Set() {
+        int[] evolutionaryTree = { -1, 0, 0, 2, 2, 4, 4, 6, 6 };
+        int deadCell = 4;
+        Set<Integer> testSet = new HashSet<>();
+        testSet.add(1);
+        testSet.add(3);
+        assertEquals(testSet, Evolution.getSurvivingCells(evolutionaryTree, deadCell));
+    }
+
+
 
     // Deliberately no tests have been provided for the method that returns a Set
     // Create a few tests of your own!
